9ea0f4f0e008080f85fbf6d1a8ff502cb0a96e64
Patryk-Zygmunt <patrykzygmuntzx@gmail.com>
2020-01-05 18:05:01 +0100

    Initial Commit

---
+++ PlaceStory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaceStory
+//
+//  Created by user163099 on 1/5/20.
+//  Copyright © 2020 user163099. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
